33c2c93d29964150f633ed11d2cbbce553442745
Anri <anrihunanyan@gmail.com>
2021-06-21 00:39:54 +0400

    feat(*): created delete button

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ function addName() {
   if (input === "") {
     return;
   }
+  const div = document.createElement("div");
+  div.setAttribute("class", "item");
+  list.appendChild(div);
+
   const p = document.createElement("p");
   const text = document.createTextNode(input);
   p.appendChild(text);
-  list.appendChild(p);
-//   input = "";
+  div.appendChild(p);
+  //   input = "";
+
+  const button = document.createElement("button");
+  button.setAttribute("class", "delete");
+  const del = document.createTextNode("Delete");
+  button.appendChild(del);
+  div.appendChild(button);
 }
 
 add.addEventListener("click", addName);
+
+// let items = document.querySelectorAll(".item");
+//   items.forEach((el, i) => {
+//     const del = document.querySelector(".delete");
+//     del.addEventListener("click", () => {
+//       items.splice(i, 1);
+//     });
+//   });
