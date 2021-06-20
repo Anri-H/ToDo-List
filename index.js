ce09b2bdd2cc9a00dcd8ca1c32a9eef2816555e1
Anri <anrihunanyan@gmail.com>
2021-06-20 23:09:36 +0400

    feat(style.css): created style.css file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const lists = document.querySelector(".list");
+const list = document.querySelector(".list");
 const add = document.querySelector(".add");
 
 function addName() {
   let input = document.querySelector(".name").value;
-  if (input.value === "") {
+  if (input === "") {
     return;
   }
   const p = document.createElement("p");
   const text = document.createTextNode(input);
   p.appendChild(text);
-  lists.appendChild(p);
-  input = "";
+  list.appendChild(p);
+//   input = "";
 }
 
 add.addEventListener("click", addName);
